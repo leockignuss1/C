a1d5bc3ed50c4071d5fc589677b2fd1d611c9ef9
leockignuss1 <leockignuss1@gmail.com>
2025-04-17 20:46:06 -0300

    Questao 1 feita!

--- Trabalhos/lista 3/Questao1.c
+++ Trabalhos/lista 3/Questao1.c
@@ -2,7 +2,22 @@
 
 int main(){
 
+    int numero, i, quantidade_div;
 
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+
+    printf("é possivel dividir %d por: ", numero);
+    for(i = 1; i <= numero; i++){
+
+        if(numero % i == 0){
+            printf("%d, ", i);
+            quantidade_div++;
+        }
+
+    }
+
+    printf("\nQuantidade de divisores: %d", quantidade_div);
 
     return 0;
 }
