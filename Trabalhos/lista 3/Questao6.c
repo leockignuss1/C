9b6815686e62038b7b615c2b644b4ab412de6533
leockignuss1 <leockignuss1@gmail.com>
2025-04-18 12:57:22 -0300

    Correção Questao-6

--- Trabalhos/lista 3/Questao6.c
+++ Trabalhos/lista 3/Questao6.c
@@ -7,14 +7,20 @@ int main(){
     printf("Digite numeros inteiros para multiplicar x e y: ");
     scanf("%d%d", &x, &y);
 
+    if(y >= 2){
     z = x + x;
     result = z;
 
-    for(teste = 3; teste <= y; teste++){
-        result = result + x;
+        for(teste = 3; teste <= y; teste++){
+            result = result + x;
+        }
+        printf("%d . %d = %d ", x, y, result);
     }
 
-    printf("%d . %d = %d ", x, y, result);
+    else{
+        printf("%d . %d = %d", x, y, x);
+    }
+   
 
     return 0;
 }
